665c34ca4ebd6e16f93dab3a55a3ea303f9ba5e9
MaxCoppen <44031065+mxcop@users.noreply.github.com>
2022-12-05 14:36:42 +0100

    wip: day 3

---
+++ puzzles/day-03/src/lib.rs
@@ -0,0 +1,29 @@
+/// 
+pub fn part_one(input: &str) -> u32 {
+  0
+}
+
+/// 
+pub fn part_two(input: &str) -> u32 {
+  0
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn part_one_works() {
+    let score = part_one(INPUT);
+    assert_eq!(score, 0);
+  }
+
+  #[test]
+  fn part_two_works() {
+    let score = part_two(INPUT);
+    assert_eq!(score, 0);
+  }
+
+  const INPUT: &str = "";
+
+}
